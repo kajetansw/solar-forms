469b89c1af559634b703d05f3908853d578abfbd
kswiatek <kajetan.swiatek@gmail.com>
2022-02-09 23:18:59 +0100

    printed form values on screen for testing and fixed issue with values not rendering (object spread instead of `produce` when setting signal)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { Component, createRenderEffect, createSignal, onCleanup, Signal } from 'solid-js';
-import type { JSX } from 'solid-js';
-import { produce } from 'solid-js/store';
+import { createRenderEffect, createSignal, onCleanup } from 'solid-js';
+import type { JSX, Component, Signal } from 'solid-js';
 import getFormControlName from './utils/get-form-control-name';
 import { FormGroupInvalidKeyError } from './utils/errors';
 import isArrayElement from './utils/is-array-element';
@@ -8,7 +7,7 @@ import isArrayElement from './utils/is-array-element';
 declare module 'solid-js' {
   namespace JSX {
     interface Directives {
-      formGroup: Signal<FormGroupState>;
+      formGroup: Signal<FormGroupValue>;
     }
 
     interface InputHTMLAttributes<T> {
@@ -17,16 +16,16 @@ declare module 'solid-js' {
   }
 }
 
-type FormGroupState = Record<string, string>;
+type FormGroupValue = Record<string, string>;
 
-function createFormGroup<T extends FormGroupState>(initialValue: T): Signal<T> {
+function createFormGroup<T extends FormGroupValue>(initialValue: T): Signal<T> {
   const signal = createSignal(initialValue);
   return signal;
 }
 
 function formGroup(
   el: JSX.FormHTMLAttributes<HTMLFormElement>,
-  formGroupSignal: () => Signal<FormGroupState>
+  formGroupSignal: () => Signal<FormGroupValue>
 ) {
   if (el && isArrayElement(el.children)) {
     const [getFormGroup, setFormGroup] = formGroupSignal();
@@ -45,11 +44,7 @@ function formGroup(
             child.value = getFormGroup()[formControlName];
           });
           const onInput = () => {
-            setFormGroup(
-              produce((s) => {
-                s[formControlName] = child.value;
-              })
-            );
+            setFormGroup((s) => ({ ...s, [formControlName]: child.value }));
           };
           child.addEventListener('input', onInput);
           onCleanup(() => child.removeEventListener('input', onInput));
@@ -64,12 +59,18 @@ const App: Component = () => {
     firstName: '',
   });
 
+  // TODO for testing - remove when unnecessary
+  // setInterval(() => console.log(form()), 1000);
+
   return (
     <>
+      <p>First name: {form().firstName}</p>
+      <hr />
       <form use:formGroup={[form, setForm]}>
-        <input type="text" formControlName="firstName" />
+        <label htmlFor="firstName">First name</label>
+        <input id="firstName" type="text" formControlName="firstName" />
       </form>
-      <button onClick={() => setForm({ firstName: 'test' })}>CLICK</button>
+      <button onClick={() => setForm((s) => ({ ...s, firstName: 'test' }))}>Change firstName</button>
     </>
   );
 };
