fc54e1eb9ed95314cc994f349ca847cb2b584425
kswiatek <kajetan.swiatek@gmail.com>
2022-02-25 10:46:58 +0100

    :wrench: added global module extension for 'solid-js' to pass TS type errors for new element attributes and directives

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 export * from './create-form-group';
 export * from './form-group-directive';
 export * from './types';
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface Directives {
+      // The only way for the directive to work without type errors
+      formGroup?: [() => any, (v: any) => any];
+    }
+
+    interface InputHTMLAttributes<T> {
+      formControlName?: string;
+    }
+  }
+}
