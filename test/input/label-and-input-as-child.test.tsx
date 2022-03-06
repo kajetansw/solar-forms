4d20d71862d61cc4cdfa0ae54e028074a80de587
kswiatek <kajetan.swiatek@gmail.com>
2022-03-06 16:35:50 +0100

    :white_check_mark: added tests for input element nested in a label element

---
+++ test/input/label-and-input-as-child.test.tsx
@@ -0,0 +1,93 @@
+import { createFormGroup, formGroup } from '../../src';
+import { screen, render, fireEvent } from 'solid-testing-library';
+import userEvent from '@testing-library/user-event';
+import getRandomString from '../utils/get-random-string';
+
+const INIT_INPUT_VALUE = getRandomString() as string | null;
+const TEST_INPUT_VALUE = getRandomString();
+const NULL = String(null);
+
+const TestApp = () => {
+  const [form, setForm] = createFormGroup({
+    valueString: INIT_INPUT_VALUE,
+    valueNull: null,
+  });
+
+  return (
+    <>
+      <p data-testid="value">{form().valueString}</p>
+      <p data-testid="value-null">{String(form().valueNull)}</p>
+      <form use:formGroup={[form, setForm]}>
+        <label for="valueString">
+          valueString
+          <input data-testid="input" id="valueString" type="text" formControlName="valueString" />
+        </label>
+        <label for="valueNull">
+          valueNull
+          <input data-testid="input-null" id="valueNull" type="text" formControlName="valueNull" />
+        </label>
+      </form>
+      <button data-testid="btn" onClick={() => setForm((s) => ({ ...s, valueString: TEST_INPUT_VALUE }))}>
+        Change valueString
+      </button>
+      <button data-testid="btn-null" onClick={() => setForm((s) => ({ ...s, valueString: null }))}>
+        Change valueString to null
+      </button>
+    </>
+  );
+};
+
+describe('Label with input element as child', () => {
+  let $valueString: HTMLElement;
+  let $valueNull: HTMLElement;
+  let $inputWithString: HTMLInputElement;
+  let $inputWithNull: HTMLInputElement;
+  let $changeToStringButton: HTMLElement;
+  let $changeToNullButton: HTMLElement;
+
+  beforeEach(async () => {
+    render(() => <TestApp />);
+
+    $valueString = await screen.findByTestId('value');
+    $valueNull = await screen.findByTestId('value-null');
+    $inputWithString = (await screen.findByTestId('input')) as HTMLInputElement;
+    $inputWithNull = (await screen.findByTestId('input-null')) as HTMLInputElement;
+    $changeToStringButton = await screen.findByTestId('btn');
+    $changeToNullButton = await screen.findByTestId('btn-null');
+  });
+
+  describe('should init value with the one provided in createFormGroup', () => {
+    it('when value is string', () => {
+      expect($valueString.innerHTML).toBe(INIT_INPUT_VALUE);
+      expect($inputWithString.value).toBe(INIT_INPUT_VALUE);
+    });
+
+    it('when value is null', () => {
+      expect($valueNull.innerHTML).toBe(NULL);
+      expect($inputWithNull.value).toBe('');
+    });
+  });
+
+  describe('should update form value when updating programmatically from outside the form', () => {
+    it('with string', () => {
+      userEvent.click($changeToStringButton);
+
+      expect($valueString.innerHTML).toBe(TEST_INPUT_VALUE);
+      expect($inputWithString.value).toBe(TEST_INPUT_VALUE);
+    });
+
+    it('with null', () => {
+      userEvent.click($changeToNullButton);
+
+      expect($valueString.innerHTML === NULL || $valueString.innerHTML === '').toBeTruthy();
+      expect($inputWithString.value).toBe('');
+    });
+  });
+
+  it('should update form value when on manual input', () => {
+    fireEvent.change($inputWithString, { target: { value: '' } });
+    userEvent.type($inputWithString, TEST_INPUT_VALUE);
+
+    expect($valueString.innerHTML).toBe(TEST_INPUT_VALUE);
+  });
+});
