cd1e4883e94389d4c50e807a42a00277c79aafca
kswiatek <kajetan.swiatek@gmail.com>
2022-04-01 19:29:01 +0200

    test: added test for support for `dirtyAll` form group property

---
+++ test/input-element/form-control-dirty-all.test.tsx
@@ -0,0 +1,103 @@
+import { createFormGroup, formGroup } from '../../src';
+import { screen, render } from 'solid-testing-library';
+import userEvent from '@testing-library/user-event';
+import getRandomString from '../utils/get-random-string';
+
+const INIT_VALUE = getRandomString();
+const TEST_VALUE = getRandomString();
+
+const TestApp = () => {
+  const fg = createFormGroup({
+    value1: INIT_VALUE,
+    value2: {
+      value21: INIT_VALUE,
+    },
+  });
+  const [dirty, setDirty] = fg.dirty;
+  const [dirtyAll, setDirtyAll] = fg.dirtyAll;
+
+  return (
+    <>
+      <p data-testid="value-dirtyAll">{JSON.stringify(dirtyAll())}</p>
+
+      <form use:formGroup={fg}>
+        <label for="value1">value1</label>
+        <input data-testid="input-value1" id="value1" type="text" formControlName="value1" />
+
+        <div formGroupName="value2">
+          <label for="value21">value21</label>
+          <input data-testid="input-value21" id="value21" type="text" formControlName="value21" />
+        </div>
+      </form>
+
+      <button data-testid="btn-mark-all-dirty" onClick={() => setDirtyAll(true)}>
+        Mark all as dirty
+      </button>
+      <button data-testid="btn-mark-all-pristine" onClick={() => setDirtyAll(false)}>
+        Mark all as pristine
+      </button>
+      <button
+        data-testid="btn-mark-each-dirty"
+        onClick={() => setDirty({ ...dirty(), value1: true, value2: { value21: true } })}
+      >
+        Mark each as dirty
+      </button>
+    </>
+  );
+};
+
+describe('Marking all form controls and groups as dirty or pristine', () => {
+  let $valueDirtyAll: HTMLElement;
+  let $inputValue1: HTMLInputElement;
+  let $inputValue21: HTMLInputElement;
+  let $btnMarkAllDirty: HTMLElement;
+  let $btnMarkAllPristine: HTMLElement;
+  let $btnMarkEachDirty: HTMLElement;
+
+  beforeEach(async () => {
+    render(() => <TestApp />);
+
+    $valueDirtyAll = await screen.findByTestId('value-dirtyAll');
+    $inputValue1 = (await screen.findByTestId('input-value1')) as HTMLInputElement;
+    $inputValue21 = (await screen.findByTestId('input-value21')) as HTMLInputElement;
+    $btnMarkAllDirty = await screen.findByTestId('btn-mark-all-dirty');
+    $btnMarkAllPristine = await screen.findByTestId('btn-mark-all-pristine');
+    $btnMarkEachDirty = await screen.findByTestId('btn-mark-each-dirty');
+  });
+
+  it('should read dirtyAll value as "false" initially', () => {
+    expect($valueDirtyAll.innerHTML).toBe(String(false));
+  });
+
+  it('should read dirtyAll value as "false" when at least one form control is pristine (not dirty)', () => {
+    userEvent.type($inputValue1, TEST_VALUE);
+
+    expect($valueDirtyAll.innerHTML).toBe(String(false));
+  });
+
+  it('should read dirtyAll value as "true" when all controls were changed from UI', () => {
+    userEvent.type($inputValue1, TEST_VALUE);
+    userEvent.type($inputValue21, TEST_VALUE);
+
+    expect($valueDirtyAll.innerHTML).toBe(String(true));
+  });
+
+  it('should read dirtyAll value as "true" when all controls were marked as dirty programmatically from outside the form', () => {
+    userEvent.click($btnMarkEachDirty);
+
+    expect($valueDirtyAll.innerHTML).toBe(String(true));
+  });
+
+  it('should disable all form controls when setting the property programmatically from outside the form', () => {
+    userEvent.click($btnMarkAllDirty);
+
+    expect($valueDirtyAll.innerHTML).toBe(String(true));
+  });
+
+  it('should mark all form controls as pristine (not dirty) when setting the property programmatically from outside the form', () => {
+    userEvent.click($btnMarkAllDirty);
+    userEvent.click($btnMarkAllPristine);
+
+    expect($valueDirtyAll.innerHTML).toBe(String(false));
+  });
+});
