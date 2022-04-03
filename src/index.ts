e7e2f4f717227af6019bf7551aa35250fa7df89f
kswiatek <kajetan.swiatek@gmail.com>
2022-04-03 16:58:53 +0200

    config: updated Solid's module extension for form group directive

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,12 @@ export * from './core/create-form-group';
 export * from './core/form-group-directive';
 export * from './types';
 
-type Accessor = () => any;
-type Setter = (v: any) => any;
-type Signal = [Accessor, Setter];
-
 declare module 'solid-js' {
   namespace JSX {
     interface Directives {
       // The only way for the directive to work without type errors
-      formGroup?: {
-        value: Signal;
-        disabled: Signal;
-      };
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      formGroup?: {};
     }
 
     interface InputHTMLAttributes<T> {
