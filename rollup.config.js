88eab22698c1c4dc65f10c2eb2eabf1d192a5bc6
kswiatek <kajetan.swiatek@gmail.com>
2022-04-03 16:59:56 +0200

    config: added `rollup-plugin-terser` and `rollup-plugin-peer-deps-external` packages to optimize bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import withRollup from 'rollup-preset-solid';
 import filesize from 'rollup-plugin-filesize';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default withRollup({
   input: 'src/index.ts',
-  plugins: [filesize()],
+  plugins: [peerDepsExternal(), terser(), filesize()],
 });
